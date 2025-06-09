d8f0416ba1173fd9ed34b7aa4ff2c91158122117
Yikexuan <13797426886@163.com>
2025-06-09 18:17:55 +0800

    基本功能全已实现

--- store/index.js
+++ store/index.js
@@ -1,7 +1,19 @@
-import { createPinia } from 'pinia'
+import {
+	defineStore
+} from 'pinia'
+import {
+	ref
+} from 'vue'
 
-const pinia = createPinia()
-
-export default pinia
+export const userStore = defineStore('user', () => {
+	const username = ref('')
+	const password = ref('')
+	
+	const setUserInfo = (userinfo) => {
+		username.value = userinfo.username
+		password.value = userinfo.password
+	}
+	return {username, password, setUserInfo}
+})
 
 //store旨在存储全局数据，让不同组件可以随时访问，而不必层层传递 props 或 emit
